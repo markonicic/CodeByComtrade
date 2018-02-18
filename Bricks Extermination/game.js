3ce91d1930f924d1d3001bc30e8a7c65ca7a9ce2
markonicic <33758868+markonicic@users.noreply.github.com>
2018-02-18 23:31:11 +0100

    Update game.js

--- Bricks Extermination/game.js
+++ Bricks Extermination/game.js
@@ -2,136 +2,152 @@ var canvas = document.getElementById("interface");
 var context = interface.getContext("2d");
 
 var ballX = interface.width / 2;
-    ballY = interface.height - 80,
-    ballRadius = 7, 
-    speedX = 3.8,
-    speedY = -3.8,
+ballY = interface.height - 80,
+  ballRadius = 7,
+  speedX = 3.8,
+  speedY = -3.8,
+  speedUp = 0.05,
 
-    stickHeight = 15,
-    stickWidth = 120,
-    stickX = (interface.width - stickWidth) / 2,
-    stickY = interface.height - stickHeight - 5,
+  stickHeight = 15,
+  stickWidth = 120,
+  stickX = (interface.width - stickWidth) / 2,
+  stickY = interface.height - stickHeight - 5,
 
-    brickRows = 7,
-    brickColumns = 9,
-    brickWidth = 70,
-    brickHeight = 20,
-    brickPadding = 7,
-    brickOffsetTop = 30,
-    brickOffsetLeft = 60,
+  brickRows = 7,
+  brickColumns = 9,
+  brickWidth = 70,
+  brickHeight = 20,
+  brickPadding = 7,
+  brickOffsetTop = 30,
+  brickOffsetLeft = 60,
 
-    count = brickRows * brickColumns,
-    remain = count,
+  count = brickRows * brickColumns,
+  lastCount = count,
+  remain = count,
 
-    score = 0,
-    lives =3;
-    
-  var bricks = [];
-  for(c=0; c<brickColumns; ++c){
-    bricks[c] = [];
-    for(r=0; r<brickRows; ++r){
-      bricks[c][r] = {x:0, y:0, status:1};
+  score = 0,
+  lives = 3;
+
+  
+var createBricks = function(brickColumns, brickRows) {
+  var b = [];
+  for (c = 0; c < brickColumns; ++c) {
+    b[c] = [];
+    for (r = 0; r < brickRows; ++r) {
+      b[c][r] = { x: 0, y: 0, status: 1 };
     }
   }
+  return b;
+}
+var bricks = createBricks(brickColumns, brickRows);
 
-
-var drawBall = function() { 
+var drawBall = function () {
   context.beginPath();
-  context.arc(ballX, ballY, ballRadius, 0, Math.PI*2);
+  context.arc(ballX, ballY, ballRadius, 0, Math.PI * 2);
   context.fillStyle = "#fff";
   context.fill();
   context.closePath();
 }
- 
-var drawStick = function() { 
+
+var drawStick = function () {
   context.beginPath();
   context.rect(stickX, stickY, stickWidth, stickHeight);
   context.fillStyle = "#0000FF";
   context.fill();
-  context.closePath(); 
+  context.closePath();
 }
- 
-var drawBricks = function() {
-  for(c=0; c<brickColumns; ++c) {
-    for(r=0; r<brickRows; ++r) {
-       if(bricks[c][r].status == 1) {
-         var brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft,
-             brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
-             bricks[c][r].x = brickX;
-             bricks[c][r].y = brickY;
-         context.beginPath();
-         context.rect(brickX, brickY, brickWidth, brickHeight);
-         if(c%2 != 0)
-           context.fillStyle = "#ffbf00";
-         else
-           context.fillStyle = "#2aa774";
-         context.fill();
-         context.closePath();
+
+var drawBricks = function () {
+  for (c = 0; c < brickColumns; ++c) {
+    for (r = 0; r < brickRows; ++r) {
+      if (bricks[c][r].status == 1) {
+        var brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft,
+          brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
+        bricks[c][r].x = brickX;
+        bricks[c][r].y = brickY;
+        context.beginPath();
+        context.rect(brickX, brickY, brickWidth, brickHeight);
+        if (c % 2 != 0)
+          context.fillStyle = "#ffbf00";
+        else
+          context.fillStyle = "#2aa774";
+        context.fill();
+        context.closePath();
       }
     }
-  } 
+  }
 }
 
-var impactDetector = function() { 
-  for(c=0; c<brickColumns; ++c){ 
-    for(r=0; r<brickRows; ++r){ 
-       var b = bricks[c][r]; 
-       if(b.status == 1){ 
-          if((ballX > b.x) && (ballX < (b.x + brickWidth)) && (ballY > b.y) && (ballY < b.y + brickHeight)){
-             speedY = -speedY;
-             b.status = 0;
-             score++;
-             count--;
-                  if(count == 0){
-                     clearInterval();
-                     alert("GREAT JOB CHAMPION, YOU WON !!!");
-                     document.location.reload();
-                  }
-          } 
-       } 
-    } 
-  } 
-}
- 
-var drawScore = function() { 
-   context.font = "20px Lucida Console";
-   context.fillStyle = "#fff";
-   context.fillText("score: "+score,80,20);
-}
- 
-var drawLives = function() {
-    context.font = "20px Lucida Console";
-    context.fillStyle = "#fff";
-    context.fillText("Bricks Extermination        lives: "+lives, interface.width-510, 20);
+var level = function (count) {
+  if (lastCount == count) {
+    return;
+  }
+  lastCount = count;
+  speedX = speedX >= 0 ? speedX + speedUp : speedX - speedUp;
+  speedY = speedY >= 0 ? speedY + speedUp : speedY - speedUp;
 }
 
-var ballMovement = function() {
-  if((ballY + speedY) < ballRadius) {
+var impactDetector = function () {
+  for (c = 0; c < brickColumns; ++c) {
+    for (r = 0; r < brickRows; ++r) {
+      var b = bricks[c][r];
+      if (b.status == 1) {
+        if ((ballX > b.x) && (ballX < (b.x + brickWidth)) && (ballY > b.y) && (ballY < b.y + brickHeight)) {
+          speedY = -speedY;
+          b.status = 0;
+          score++;
+          count--;
+          level(count);
+          if (count == 0) {
+            clearInterval(drawInterval);
+            alert("GREAT JOB CHAMPION, YOU WON !!!");
+            document.location.reload();
+          }
+        }
+      }
+    }
+  }
+}
+
+var drawScore = function () {
+  context.font = "20px Lucida Console";
+  context.fillStyle = "#fff";
+  context.fillText("score: " + score, 80, 20);
+}
+
+var drawLives = function () {
+  context.font = "20px Lucida Console";
+  context.fillStyle = "#fff";
+  context.fillText("Bricks Extermination        lives: " + lives, interface.width - 510, 20);
+}
+
+var ballMovement = function () {
+  if ((ballY + speedY) < ballRadius) {
     speedY = -speedY;
   }
-  else if((ballY + speedY + 15) > (interface.height - ballRadius)) { 
-     if((ballX >= stickX) && (ballX <= stickX + stickWidth)) { 
-        speedY = -speedY;
-     }
-     else {
-        lives--;
-        if(!lives) {
-          clearInterval();
-          alert("Sorry, no more lifes left...\nTry again!");
-          document.location.reload(); 
-        }
-        else {
-          ballX = interface.width / 2;
-          ballY = interface.height - 20;
-          remain = count;
-          stickX = (interface.width - stickWidth) / 2;
-         }
-    } 
+  else if ((ballY + speedY + 15) > (interface.height - ballRadius)) {
+    if ((ballX >= stickX) && (ballX <= stickX + stickWidth)) {
+      speedY = -speedY;
+    }
+    else {
+      lives--;
+      if (!lives) {
+        clearInterval(drawInterval);
+        alert("Sorry, no more lifes left...\nTry again!");
+        document.location.reload();
+      }
+      else {
+        ballX = interface.width / 2;
+        ballY = interface.height - 20;
+        remain = count;
+        stickX = (interface.width - stickWidth) / 2;
+      }
+    }
   }
   else {
     ballY += speedY;
   }
-  if((ballX + speedX < ballRadius) || (ballX + speedX > interface.width - ballRadius)) {
+  if ((ballX + speedX < ballRadius) || (ballX + speedX > interface.width - ballRadius)) {
     speedX = -speedX;
   }
   else {
@@ -139,25 +155,25 @@ var ballMovement = function() {
   }
 }
 
-var draw = function() { 
-    context.clearRect(0 , 0, interface.width, interface.height);
-    drawBricks();
-    drawBall();
-    drawStick();
-    drawScore();
-    drawLives();
-    impactDetector();
-    ballMovement();
+var draw = function () {
+  context.clearRect(0, 0, interface.width, interface.height);
+  drawBricks();
+  drawBall();
+  drawStick();
+  drawScore();
+  drawLives();
+  impactDetector();
+  ballMovement();
 }
 
-var mouseMoveHandler = function(e) { 
-  var relativeX = e.clientX - interface.offsetLeft; 
-  if((relativeX > 0) && (relativeX < interface.width)) { 
-     if((relativeX - stickWidth/2 >= 0) && (relativeX-stickWidth/2 <= interface.width-stickWidth))
-        stickX = relativeX - stickWidth/2; 
-  } 
+var mouseMoveHandler = function (e) {
+  var relativeX = e.clientX - interface.offsetLeft;
+  if ((relativeX > 0) && (relativeX < interface.width)) {
+    if ((relativeX - stickWidth / 2 >= 0) && (relativeX - stickWidth / 2 <= interface.width - stickWidth))
+      stickX = relativeX - stickWidth / 2;
+  }
 }
 
-document.addEventListener("mousemove", mouseMoveHandler, false); 
- 
-setInterval(draw, 20);
+document.addEventListener("mousemove", mouseMoveHandler, false);
+
+var drawInterval = setInterval(draw, 20);
